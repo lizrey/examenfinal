e10e7cddbc5a15193243a60b3bfd934b8aea7d80
lizrey <69904028+lizrey@users.noreply.github.com>
2020-09-13 18:34:46 -0500

    Update Untitled-1.py

--- static/js/Untitled-1.py
+++ static/js/Untitled-1.py
@@ -3,8 +3,6 @@ import time
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
-
-
 GPIO.setmode(GPIO.BCM)
 
 pulsador=5
@@ -12,44 +10,44 @@ led=12
 pulsador1=6
 led1=13
 
+            
 def main():
+    usuario="licha_05reyes@outlook.com"
+    password="Galapagos1001"
+    Stopic="licha_05reyes@outlook.com/IoT1"
+    SendTopic="licha_05reyes@outlook.com/IoT"
     GPIO.setup(pulsador,GPIO.IN)
     GPIO.setup(led,GPIO.OUT)
     GPIO.setup(pulsador1,GPIO.IN)
     GPIO.setup(led1,GPIO.OUT)
 
     mqttc=mqtt.Client()
-    mqttc.on_message=on_message
-    mqttc.username_pw_set("licha_05reyes@outlook.com","Galapagos1001")
+    mqttc.username_pw_set(usuario,password)
     mqttc.connect("maqiatto.com",1883)
-    mqttc.subscribe("licha_05reyes@outlook.com/IoT1",0)
-    info=""
+    mqttc.subscribe(Stopic,0)
+    info="0"
     while(1):
-        cont=1
         mqttc.loop()
         f=open("informacion.txt","a")
         info=str(datetime.now())
         if(GPIO.input(pulsador)==0):
-            f.write("- "+info+" se ha presionado el pulsador uno \n")
-            f.close()
-            f = open("informacion.txt", "r")
-            f.read()
+            f.write("- "+info+" se ha presionado el pulsador uno\n")
+            f=open("informacion.txt","r")
             GPIO.output(led,1)
-            mqttc.publish("licha_05reyes@outlook.com/IoT","on/off"+str(f.read()))
+            mqttc.publish(SendTopic,"on/off")
             time.sleep(1)
-            mqttc.publish("licha_05reyes@outlook.com/IoT","off/off")
-            cont=0
+            mqttc.publish(SendTopic,"off/off/"+f.read())
+            f.close()
         
         if(GPIO.input(pulsador1)==0):
             
-            f.write("- "+info+" se ha presionado el pulsador dos \n")
-            f.close()
-            f = open("informacion.txt", "r")
-            f.read()
+            f.write("- "+info+" se ha presionado el pulsador dos\n")
+            f=open("informacion.txt","r")
             GPIO.output(led1,1)
-            mqttc.publish("licha_05reyes@outlook.com/IoT","off/on")
+            mqttc.publish(SendTopic,"off/on")
             time.sleep(1)
-            mqttc.publish("licha_05reyes@outlook.com/IoT","off/off")
+            mqttc.publish(SendTopic,"off/off/"+f.read())
+            f.close()
         
         
         
@@ -58,7 +56,4 @@ def main():
         
         
         
-            
-def on_message(client,obj,msg):
-    print(msg.topic+" "+str(msg.qos)+" "+msg.payload.decode('utf-8'))
             
